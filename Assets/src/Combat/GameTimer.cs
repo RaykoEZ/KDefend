037da454d9ca249fc16f400c223f63e792744d6e
RaykoEZ <onion55zh@gmail.com>
2025-01-09 06:05:21 +0000

    Adding util and enemy logic script

---
+++ Assets/src/Combat/GameTimer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameTimer : MonoBehaviour 
+{
+    [SerializeField] TextMeshProUGUI m_secondDisplay = default;
+    [SerializeField] UnityEvent<int> m_onTimeElapsed = default;
+    bool m_isActive = false;
+    int m_secondsElapsed = 0;   
+    Coroutine m_timer;
+    IEnumerator UpdateTimer() 
+    {
+        while (m_isActive) 
+        {
+            yield return new WaitForSeconds(1f);
+            ++m_secondsElapsed;
+            
+        }
+    }
+}
--- Assets/src/Combat/Hostile.cs
+++ Assets/src/Combat/Hostile.cs
@@ -1,6 +1,61 @@
-﻿using UnityEngine;
-
+﻿using System;
+using System.Collections;
+using UnityEngine;
+[Serializable]
+public struct EnemySpawnPattern
+{
+    public int KillsForEarlySpawn;
+    public float SecondsElapsed;
+}
 public class Hostile : BaseCharacter 
 {
-    protected Transform m_toFollow;
+    [SerializeField] float m_moveInterval = default;
+    protected Transform m_target;
+    Coroutine m_movement;
+    public void Init(Transform target)
+    {
+        m_target = target;
+        StartMoving();
+    }
+    public virtual void StartAttack(Vector2 dirNormalize) 
+    {
+    }
+    protected void SwitchTarget(BaseEntity newTarget) 
+    {
+        if (newTarget == null) return;
+        m_target = newTarget.transform;
+    }
+    protected override void OnDefeat()
+    {
+        StopMoving();
+        base.OnDefeat();
+    }
+    public void StartMoving()
+    {
+        if (m_movement == null && m_target != null)
+        {
+            m_movement = StartCoroutine(Movement());
+        }
+    }
+    public void StopMoving()
+    {
+        if (m_movement != null)
+        {
+            StopCoroutine(m_movement);
+            m_movement = null;
+        }
+    }
+    IEnumerator Movement()
+    {
+        float dist = Vector3.Distance(transform.position, m_target.position);
+        float t = 0f;
+        while (dist > 0.01f)
+        {
+            t += m_moveInterval * Time.deltaTime;
+            transform.position = Vector3.Lerp(transform.position, m_target.position, t);
+            yield return new WaitForSeconds(0.5f);
+            dist = Vector3.Distance(transform.position, m_target.position);
+        }
+        m_movement = null;
+    }
 }
