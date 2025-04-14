a54b32f795419190b175efb9f64582f68d374d2a
RaykoEZ <onion55zh@gmail.com>
2025-04-14 01:54:37 +0100

    adding spawn handling

--- Assets/src/Combat/Entities/Spawn/EnemyWaveDetail.cs
+++ Assets/src/Combat/Entities/Spawn/EnemyWaveDetail.cs
@@ -6,9 +6,12 @@ using UnityEngine;
 public struct EnemySpawnItem
 {
     [SerializeField] int m_numToSpawn;
+    // if < 0, pick random spawn location from list
+    [SerializeField] int m_spawnLocationIndex;
     [SerializeField] Enemy m_spawnRef;
     public int NumToSpawn => m_numToSpawn;
     public Enemy SpawnRef => m_spawnRef;
+    public int SpawnLocationIndex => m_spawnLocationIndex;
 }
 //Contains all enemies to spawn in a wave
 [CreateAssetMenu(fileName = "Wave_", menuName = "Jams/EnemySpawning/Create a new enemy wave detail", order = 1)]
@@ -20,4 +23,23 @@ public class EnemyWaveDetail : ScriptableObject
     [SerializeField] List<EnemySpawnItem> m_groupsToSpawn = default;
     public float SecondsBeforeNextWave => m_secondsBeforeNextWave;
     public List<EnemySpawnItem> GroupsToSpawn => m_groupsToSpawn;
+}
+
+public struct SpawnId : IEquatable<SpawnId>
+{
+    int m_location;
+    int m_indexInList;
+    public int Location { get => m_location; }
+    public int IndexInList { get => m_indexInList; }
+    public bool Equals(SpawnId other)
+    {
+        return Location == other.Location && IndexInList == other.IndexInList;
+    }
+}
+public class SpawnContainer : MonoBehaviour 
+{
+
+    Dictionary<int, List<EnemySpawnItem>> m_spawnsByLocation = new Dictionary<int, List<EnemySpawnItem>>();
+    // use for timer spawns
+    Dictionary<int, List<SpawnId>> m_spawnBytimeFrame = new Dictionary<int, List<SpawnId>>();
 }
--- Assets/src/Events/KDefenderTimerEvent.cs
+++ Assets/src/Events/KDefenderTimerEvent.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 // Concrete timer event
 [Serializable]
-public struct KDefenderTimerEvent : ITimerEvent<KDefenderEventContext>
+public class KDefenderTimerEvent : ITimerEvent<KDefenderEventContext>
 {
-    [SerializeField] int m_triggerTime;
+    [SerializeField] protected int m_triggerTime;
     [SerializeField] UnityEvent<KDefenderEventContext> m_toInvoke;
     public UnityEvent<KDefenderEventContext> ToInvoke => m_toInvoke;
     public int TriggerTime => m_triggerTime;
