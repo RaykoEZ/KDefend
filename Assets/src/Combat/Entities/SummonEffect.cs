b07f419fb4faa3f18065a7dfa72cfd16e2028857
RaykoEZ <onion55zh@gmail.com>
2025-11-16 03:28:30 +0000

    Tweaked striker call setup

--- Assets/src/Combat/Entities/SummonEffect.cs
+++ Assets/src/Combat/Entities/SummonEffect.cs
@@ -2,10 +2,16 @@
 
 public class SummonEffect : EffectModule
 {
+    [Range(1, 99)]
+    [SerializeField] int m_numToSummon = default;
     [SerializeField] SummonedEntity m_toSpawn = default;
     public override void Activate(BaseEntity target)
     {
-        var instance = GameUtil.SpawnObject(m_toSpawn, transform.position, transform.parent);
-        instance?.Init();
+        SummonedEntity instance;
+        for (int i = 0; i < m_numToSummon; i++)
+        {
+            instance = GameUtil.SpawnObject(m_toSpawn, transform.position, transform.parent);
+            instance?.Init();
+        }
     }
 }
