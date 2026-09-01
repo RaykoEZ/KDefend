6df13d1cf36840e4b10e88271f560815d66ba272
RaykoEZ <onion55zh@gmail.com>
2026-09-01 16:45:04 +0100

    Adding dialogue events and content for chracter: Tempo, Narrator, ???

---
+++ Assets/src/Plugins/Dialogue/DialogueLua_OnRetreat.cs
@@ -0,0 +1,30 @@
+﻿using PixelCrushers.DialogueSystem;
+using UnityEngine;
+
+public class DialogueLua_OnRetreat : MonoBehaviour
+{
+    [Tooltip("Typically leave unticked so temporary Dialogue Managers don't unregister your functions.")]
+    public bool unregisterOnDisable = false;
+    [SerializeField] DialogueDatabase m_database = default;
+    string m_spawnFunctionID => nameof(OnRetreat);
+
+    void OnEnable()
+    {
+        // Make spawn function ID bound to each unique spawn wave 
+        Lua.RegisterFunction(m_spawnFunctionID, this, SymbolExtensions.GetMethodInfo(() => OnRetreat()));
+    }
+    void OnDisable()
+    {
+        if (unregisterOnDisable)
+        {
+            // Remove the functions from Lua: (Replace these lines with your own.)
+            Lua.UnregisterFunction(m_spawnFunctionID);
+        }
+    }
+    public void OnRetreat()
+    {
+        int num = DialogueLua.GetVariable("NumRetreat").AsInt;
+        DialogueLua.SetVariable("NumRetreat", num++);
+    }
+}
+/**/
